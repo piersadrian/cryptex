b654602f4515eef70ed70eb6f23f1303f8ab33a2
Piers Mainwaring <piers@impossibly.org>
2017-12-12 08:46:13 -0700

    Improve freq analysis API

--- lib/analyze.ex
+++ lib/analyze.ex
@@ -6,38 +6,37 @@ defmodule Analyze do
         iex> Analyze.Frequency.attempt_decrypt("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
         "Cooking MC's like a pound of bacon"
     """
-    def attempt_decrypt(hex) do
-      attempt_all_single_char_keys(hex, String.length(hex))
+    def attempt_decrypt(ciphertext) do
+      Enum.map(?A..?z, fn char ->
+        with plaintext <- single_char_decrypt(ciphertext, char),
+             score <- score_plaintext(plaintext),
+             do: {char, score, plaintext}
+      end)
       |> Enum.sort_by(fn tuple -> elem(tuple, 1) end, &>=/2)
       |> hd()
       |> elem(2)
     end
 
-    @frequency_chars ' eEtTaAoOiInNsShHrRdDlLcCuUmMwWfFgGyYpPbBvVkKjJxXqQzZ'
-
-    defp attempt_all_single_char_keys(cipher_hex, key_size) do
-      Enum.map(?A..?z, fn char ->
-        plaintext =
-          Integer.to_string(char, 16)
-          |> Util.repeating(key_size)
-          |> Bits.xor(cipher_hex)
-          |> Base.decode16!()
-
-        score = score_candidate(plaintext)
-        {List.to_string([char]), score, plaintext}
-      end)
+    defp single_char_decrypt(ciphertext, single_char_key) do
+      single_char_key
+      |> Integer.to_string(16)
+      |> Util.repeating(String.length(ciphertext))
+      |> Bits.xor(ciphertext)
+      |> Base.decode16!()
     end
 
-    defp score_candidate(plaintext) do
+    @frequency_chars ' eEtTaAoOiInNsShHrRdDlLcCuUmMwWfFgGyYpPbBvVkKjJxXqQzZ'
+
+    defp score_plaintext(plaintext) do
       @frequency_chars
       |> Enum.reverse()
       |> Enum.with_index()
       |> Enum.reduce(0, fn {char, index}, score ->
-           score + :math.pow(freq(plaintext, char), index)
+           score + :math.pow(character_frequency(plaintext, char), index)
          end)
     end
 
-    defp freq(plaintext, char) do
+    defp character_frequency(plaintext, char) do
       plaintext
       |> String.to_charlist()
       |> Enum.reduce(0, fn
