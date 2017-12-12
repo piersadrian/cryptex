5009748755acfbf29a8b5193004448ad760e7ba9
Piers Mainwaring <piers@impossibly.org>
2017-12-11 17:37:32 -0700

    Add test for frequency analysis

--- lib/analyze.ex
+++ lib/analyze.ex
@@ -1,7 +1,13 @@
 defmodule Analyze do
   defmodule Frequency do
+    @doc """
+    Performs English letter-frequency analysis to brute-force a single-character key.
+
+        iex> Analyze.Frequency.attempt_decrypt("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+        "Cooking MC's like a pound of bacon"
+    """
     def attempt_decrypt(hex) do
-      score_by_frequency(hex, String.length(hex))
+      attempt_all_single_char_keys(hex, String.length(hex))
       |> Enum.sort_by(fn tuple -> elem(tuple, 1) end, &>=/2)
       |> hd()
       |> elem(2)
@@ -9,7 +15,7 @@ defmodule Analyze do
 
     @frequency_chars ' eEtTaAoOiInNsShHrRdDlLcCuUmMwWfFgGyYpPbBvVkKjJxXqQzZ'
 
-    defp score_by_frequency(cipher_hex, key_size) do
+    defp attempt_all_single_char_keys(cipher_hex, key_size) do
       Enum.map(?A..?z, fn char ->
         plaintext =
           Integer.to_string(char, 16)
